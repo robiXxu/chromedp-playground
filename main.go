4fb6bacdd1a24c19d30b03a9fbe60e05cdfa4b13
Robert Schiriac <robert_schiriac@trimble.com>
2022-11-01 09:50:01 +0200

    updated

--- main.go
+++ main.go
@@ -10,14 +10,29 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
+func navAndShot(ctx *context.Context, url string, label string) {
+	var buf []byte
+	chromedp.Run(
+		*ctx,
+		chromedp.Navigate(url),
+		chromedp.WaitReady("body"),
+		chromedp.FullScreenshot(&buf, 90),
+	)
+
+	err := ioutil.WriteFile(fmt.Sprintf("%s.png", label), buf, 0o644)
+	if err != nil {
+		log.Fatalf("Failed to take screenshot: %+v", err)
+	}
+}
+
 func main() {
 	fmt.Println("Just a chromedp playground")
 
 	args := append(
 		chromedp.DefaultExecAllocatorOptions[:],
 
-		chromedp.Headless,
-		chromedp.DisableGPU,
+		//chromedp.Headless,
+		//chromedp.DisableGPU,
 
 		// chromedp.NoSandbox,
 		chromedp.Flag("font-render-hinting", "none"),
@@ -25,7 +40,7 @@ func main() {
 		chromedp.Flag("enable-accelerated-video-decode", true),
 		chromedp.Flag("enable-gpu-rasterization", true),
 
-		chromedp.Flag("use-gl", "swiftshader"),
+		// chromedp.Flag("use-gl", "swiftshader"),
 		chromedp.Flag("enable-threaded-compositing", true),
 
 		chromedp.Flag("allow-insecure-localhost", true),
@@ -33,7 +48,7 @@ func main() {
 		chromedp.Flag("allow-file-access-from-files", true),
 
 		chromedp.Flag("disable-gpu-compositing", true),
-		//chromedp.Flag("incognito", true),
+		chromedp.Flag("incognito", true),
 
 		chromedp.Flag("disable-gpu-watchdog", true),
 		chromedp.Flag("disable-hang-monitor", true),
@@ -48,24 +63,14 @@ func main() {
 	)
 	defer cancel()
 
-	//url := "chrome://gpu"
+	gpu := "chrome://gpu"
 
 	dirPath, _ := os.Getwd()
 	log.Println(dirPath)
 
-	url := fmt.Sprintf("file://%s/index.html", dirPath)
+	webgl := fmt.Sprintf("file://%s/index.html", dirPath)
 
-	var buf []byte
-	chromedp.Run(
-		ctx,
-		chromedp.Navigate(url),
-		chromedp.WaitReady("body"),
-		chromedp.FullScreenshot(&buf, 90),
-	)
-
-	err := ioutil.WriteFile("sc.png", buf, 0o644)
-	if err != nil {
-		log.Fatalf("Failed to take screenshot: %+v", err)
-	}
+	navAndShot(&ctx, gpu, "gpu")
+	navAndShot(&ctx, webgl, "webgl")
 
 }
